045f4dbfc59dc9def24aeb9751e149eea0ea5008
Caio Gabriel <141652044+hofkay@users.noreply.github.com>
2023-12-07 20:56:39 -0400

    Add files via upload

---
+++ index (4).php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+    $campus= "Instituto Federal";
+    echo "<p> Eu estudo no $campus!</p>";
+    ?>
+
+    <?php 
+    date_default_timezone_set("America/Sao_Paulo");
+    echo "Hoje é dia ". date("d/m/y");
+    echo "e a hora atual é ". date("G:i:s");
+    ?>
+    
+</body>
+</html>
